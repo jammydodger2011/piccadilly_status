bf1d1c63d10d82d8768aa81701e40f46d7d6adcd
James Wyatt <james.wyatt@ibm.com>
2023-08-15 08:20:02 +0100

    Edited deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # AWS Variables
-ECR_REGION="eu-west-1"
-ECR_ACCOUNT_ID="502761249685"
-ECR_REPO_NAME="piccadilly_status"
-LAMBDA_FUNCTION_NAME="arn:aws:lambda:eu-west-1:502761249685:function:ecr-picc_status"
+ECR_REGION= #Please insert
+ECR_ACCOUNT_ID= #Please insert
+ECR_REPO_NAME= #Please insert
+LAMBDA_FUNCTION_NAME= #Please insert
 
 # Authenticate Docker with Amazon ECR registry
 aws ecr get-login-password --region "$ECR_REGION" | docker login --username AWS --password-stdin "$ECR_ACCOUNT_ID".dkr.ecr."$ECR_REGION".amazonaws.com
